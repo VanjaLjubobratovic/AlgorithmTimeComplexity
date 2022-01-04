eca785ec2f37f0bb532875766db2af6ad4a05e4c
Vanja Ljubobratovic <vanja1708@gmail.com>
2022-01-04 20:58:42 +0100

    Implemented and tested dynamic fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,6 @@
 def fib_non_dynamic(n):
-    if n == 0 or n == 1:
+    if n <= 1:
         return 1
-    if n == 2:
-        return 2
     return fib_non_dynamic(n - 1) + fib_non_dynamic(n - 2)
 
 def fib_non_dynamic_list(n):
@@ -10,3 +8,12 @@ def fib_non_dynamic_list(n):
     for i in range(n):
         fibo_vals.append(fib_non_dynamic(i))
     return fibo_vals
+
+def fib_dynamic(n, list):
+    arr = [1, 1]
+    for i in range(2, n):
+        arr.append(arr[i - 1] + arr[i - 2])
+    if list == True:
+        return arr
+    else:
+        return arr[-1]
--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import numpy as np
 from searchAlgo import *
 from fibonacci import *
+import time
 
 def main ():
-    ##temporary binary search test
-    print("BINARY SEARCH")
-    arr = np.random.randint(100, size=100)
-    arr[34] = 25
-    arr.sort()
-    ##print(arr)
-    print(binary_search(25, arr, 0, len(arr) - 1))
+    # ##temporary binary search test
+    # print("BINARY SEARCH")
+    # arr = np.random.randint(100, size=100)
+    # arr[34] = 25
+    # arr.sort()
+    # # print(arr)
+    # print(binary_search(25, arr, 0, len(arr) - 1))
 
-    ##temporary linear search test
-    print("LINEAR SEARCH")
-    print(linear_search(25, arr))
+    # temporary linear search test
+    # print("LINEAR SEARCH")
+    # print(linear_search(25, arr))
 
-    ##temporary fibonacci test
+    # temporary fibonacci test
+    begin = time.perf_counter()
     print("FIBONACCI NON-DYNAMIC")
-    print(fib_non_dynamic_list(20))
+    print(fib_non_dynamic_list(35))
+    # fib_non_dynamic(33)
+    end = time.perf_counter()
 
-    generate_search_testcases()
+    print(f"Time to execute: {end - begin:0.4f} s")
+
+
+    begin = time.perf_counter()
+    print("FIBONACCI DYNAMIC")
+    print(fib_dynamic(35, True))
+    end = time.perf_counter()
+
+    print(f"Time to execute: {end - begin:0.4f} s")
+
+    # generate_search_testcases()
 
 if __name__ == "__main__":
     main()
