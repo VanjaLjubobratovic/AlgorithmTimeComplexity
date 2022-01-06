9c81c6cc85ec961b38946702a70ca4657d8dbe61
IJustMa <46496195+KarloGraf@users.noreply.github.com>
2022-01-06 21:25:04 +0100

    Added matrix test case generation and test case reading from file

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -1,9 +1,11 @@
+import random
+import time
+import numpy
+import json
 def MatrixMultiplication(A, B):
     if(len(A[0]) != len(B)):
         return "Invalid matrix dimensions"
-    result = [[0, 0, 0, 0],
-		    [0, 0, 0, 0],
-		    [0, 0, 0, 0]]
+    result = [[0 for i in range(len(A[0]))] for j in range(len(B[0]))]
 
     # iterating by row of A
     for i in range(len(A)):
@@ -16,16 +18,44 @@ def MatrixMultiplication(A, B):
                 result[i][j] += A[i][k] * B[k][j]                        
     return result
 
+def Generate_random_matrix(n):
+    matrix = [[0 for i in range(n)] for j in range(n)]
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = random.randint(0, 100) 
+    return matrix
 
-# Code to test funcion above
-A = [[12, 7, 3],
-	[4, 5, 6],
-	[7, 8, 9]]
+def generate_testcases(low, high, step):
+        data = {}
+        data["Matrices"] = []
 
-B = [[5, 8, 1, 2],
-	[6, 7, 3, 0],
-	[4, 5, 9, 1]]
-	
-result = MatrixMultiplication(A,B)
-for r in result:
-	print(r)
+        while low < high:
+            size_n = low
+            g = Generate_random_matrix(size_n)
+            data["Matrices"].append({
+                "Size": size_n,
+                "Matrix": g,
+            })
+            low += step
+
+        with open("MatrixTestCases.txt", "w") as outfile:
+            json.dump(data, outfile)
+
+def Read_testcases():
+    test_cases = []
+    with open("MatrixTestCases.txt") as fp:
+        data = json.load(fp)
+    for i in data['Matrices']:
+        test_cases.append(i['Matrix'])
+    return test_cases
+
+
+
+#generate_testcases(2,50,1)
+test_cases = Read_testcases()
+for i in test_cases:
+    begin = time.perf_counter()
+    mat = MatrixMultiplication(i,i)
+    end = time.perf_counter()
+    print(f"Time to execute: {end - begin:0.7f} s")
+
