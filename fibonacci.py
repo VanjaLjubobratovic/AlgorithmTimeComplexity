06b29450a6d9d10aa4468dc3e20a1ddeac38199d
Vanja Ljubobratovic <vanja1708@gmail.com>
2022-01-07 16:54:17 +0100

    Implemented testcase generation and execution of all fibonacci algorithms.

--- fibonacci.py
+++ fibonacci.py
@@ -1,42 +1,97 @@
-MAX = 10000000
+import time
+import json
 
-def fib_non_dynamic(n):
-    if n <= 1:
-        return 1
-    return fib_non_dynamic(n - 1) + fib_non_dynamic(n - 2)
+class Fibonacci:
+    MAX = 10000000
 
-def fib_non_dynamic_list(n):
-    fibo_vals = []
-    for i in range(n):
-        fibo_vals.append(fib_non_dynamic(i))
-    return fibo_vals
+    def fib_non_dynamic(n):
+        if n <= 1:
+            return 1
+        return Fibonacci.fib_non_dynamic(n - 1) + Fibonacci.fib_non_dynamic(n - 2)
 
-def fib_dynamic(n, list):
-    arr = [1, 1]
-    for i in range(2, n):
-        arr.append(arr[i - 1] + arr[i - 2])
-    if list == True:
-        return arr
-    else:
-        return arr[-1]
+    def fib_non_dynamic_list(n):
+        fibo_vals = []
+        for i in range(n):
+            fibo_vals.append(Fibonacci.fib_non_dynamic(i))
+        return fibo_vals
 
-f = [0] * MAX
-def fib_optimized(n):
-    if n == 0:
-        return 0
-    if n == 1 or n == 2:
-        f[n] = 1
-        return f[n]
-    if (f[n]):
-        return f[n]
-    if n & 1:
-        k = (n + 1) // 2
-    else:
-        k = n // 2
-    
-    if (n & 1):
-        f[n] = (fib_optimized(k) * fib_optimized(k) + fib_optimized(k - 1) * fib_optimized(k - 1))
-    else:
-        f[n] = (2 * fib_optimized(k - 1) + fib_optimized(k)) * fib_optimized(k)
-    return f[n]
+    def fib_dynamic(n, list): #list is a bool variable used to set the return of final list
+        arr = [1, 1]
+        for i in range(2, n):
+            arr.append(arr[i - 1] + arr[i - 2])
+        if list == True:
+            return arr
+        else:
+            return arr[-1]
 
+    f = [0] * MAX
+    def fib_optimized(n):
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            Fibonacci.f[n] = 1
+            return Fibonacci.f[n]
+        if (Fibonacci.f[n]):
+            return Fibonacci.f[n]
+        if n & 1:
+            k = (n + 1) // 2
+        else:
+            k = n // 2
+        
+        if (n & 1):
+            Fibonacci.f[n] = (Fibonacci.fib_optimized(k) * Fibonacci.fib_optimized(k) + Fibonacci.fib_optimized(k - 1) * Fibonacci.fib_optimized(k - 1))
+        else:
+            Fibonacci.f[n] = (2 * Fibonacci.fib_optimized(k - 1) + Fibonacci.fib_optimized(k)) * Fibonacci.fib_optimized(k)
+        return Fibonacci.f[n]
+
+    def generate_results(min, max, step, max_recursive):
+        current_size = min
+        data = {}
+        data["Naive"] = []
+        data["Dynamic"] = []
+        data["Optimized"] = []
+
+        for i in range(1, max_recursive + 1, 1):
+            begin = time.perf_counter()
+            Fibonacci.fib_non_dynamic(i)
+            end = time.perf_counter()
+
+            data["Naive"].append({
+            "Size": i,
+            "Time": float(end - begin)
+            })
+        
+        while current_size <= max:
+            begin = time.perf_counter()
+            Fibonacci.fib_dynamic(current_size, False)
+            end = time.perf_counter()
+
+            data["Dynamic"].append({
+                "Size": current_size,
+                "Time": float(end - begin)
+            })
+
+            begin = time.perf_counter()
+            Fibonacci.fib_optimized(current_size)
+            end = time.perf_counter()
+
+            data["Optimized"].append({
+                "Size": current_size,
+                "Time": float(end - begin)
+            })
+            current_size += step
+        print("Writing data")
+        with open("FibonacciResults.txt", "w") as outfile:
+            json.dump(data, outfile)
+
+
+# privremena funkcija za testiranje algoritama
+def main():
+    print("Generating Fibonacci results")
+    begin = time.perf_counter()
+    Fibonacci.generate_results(10000, 200000, 5000, 35)
+    end = time.perf_counter()
+    print(f"Generation finished in: {end - begin:0.4f}s")
+
+if __name__ == "__main__":
+    main()
--- searchAlgo.py
+++ searchAlgo.py
@@ -3,104 +3,103 @@ import numpy as np
 import time
 import ijson
 
-
-
-def binary_search(value, arr, low, high):
-    if high >= low:
-        mid = (high + low) // 2
-        if arr[mid] == value:
-            return mid
-        elif arr[mid] > value:
-            return binary_search(value, arr, low, mid - 1)
+class SearchAlgo:
+    def binary_search(value, arr, low, high):
+        if high >= low:
+            mid = (high + low) // 2
+            if arr[mid] == value:
+                return mid
+            elif arr[mid] > value:
+                return SearchAlgo.binary_search(value, arr, low, mid - 1)
+            else:
+                return SearchAlgo.binary_search(value, arr, mid + 1, high)
         else:
-            return binary_search(value, arr, mid + 1, high)
-    else:
+            return -1
+
+    def linear_search(value, arr):
+        for i in range(0, len(arr)):
+            if arr[i] == value:
+                return i
         return -1
 
-def linear_search(value, arr):
-    for i in range(0, len(arr)):
-        if arr[i] == value:
-            return i
-    return -1
+    #OBSOLETE FUNCTION
+    def generate_search_testcases(low, high, step):
+        #High should be 10 mil max with this implementation
+        #low is intended to be 1 mil and step is 100k
+        data = {}
+        data["Searching Algorithms"] = []
 
-#OBSOLETE FUNCTION
-def generate_search_testcases(low, high, step):
-    #High should be 10 mil max with this implementation
-    #low is intended to be 1 mil and step is 100k
-    data = {}
-    data["Searching Algorithms"] = []
+        i = low
+        while i <= high:
+            print(i)
+            arr = np.random.randint(i, size=i)
+            arr.sort()
+            data["Searching Algorithms"].append({
+                "Size": i,
+                "Inputs": arr.tolist(),
+                "Value": int(np.amax(arr))
+            })
+            i += step
+        with open("SearchingAlgorithms.txt", "w") as outfile:
+            json.dump(data, outfile)
 
-    i = low
-    while i <= high:
-        print(i)
-        arr = np.random.randint(i, size=i)
+    #OBSOLETE FUNCTION
+    def read_testcases():
+        test_cases = []
+        test_case_sizes = []
+        test_case_search_values = []
+        with open("SearchingAlgorithms.txt", "rb") as infile:
+            items = ijson.items(infile, "Searching Algorithms.item")
+            for item in items:
+                test_cases.append(item["Inputs"])
+                test_case_sizes.append(item["Size"])
+                test_case_search_values.append(item["Value"])
+        return test_cases, test_case_sizes, test_case_search_values
+
+
+    def generate_array(num_of_elements):
+        arr = np.random.randint(num_of_elements, size = num_of_elements)
         arr.sort()
-        data["Searching Algorithms"].append({
-            "Size": i,
-            "Inputs": arr.tolist(),
-            "Value": int(np.amax(arr))
-        })
-        i += step
-    with open("SearchingAlgorithms.txt", "w") as outfile:
-        json.dump(data, outfile)
-
-#OBSOLETE FUNCTION
-def read_testcases():
-    test_cases = []
-    test_case_sizes = []
-    test_case_search_values = []
-    with open("SearchingAlgorithms.txt", "rb") as infile:
-        items = ijson.items(infile, "Searching Algorithms.item")
-        for item in items:
-            test_cases.append(item["Inputs"])
-            test_case_sizes.append(item["Size"])
-            test_case_search_values.append(item["Value"])
-    return test_cases, test_case_sizes, test_case_search_values
+        search_val = int(np.amax(arr))
+        return arr, search_val
 
 
-def generate_array(num_of_elements):
-    arr = np.random.randint(num_of_elements, size = num_of_elements)
-    arr.sort()
-    search_val = int(np.amax(arr))
-    return arr, search_val
+    def generate_results(min, max, step):
+        current_size = min
+        data = {}
+        data["Binary"] = []
+        data["Linear"] = []
+        while current_size <= max:
+            test_array, search_val = SearchAlgo.generate_array(current_size)
+            begin = time.perf_counter()
+            SearchAlgo.binary_search(search_val, test_array, 0, current_size - 1)
+            end = time.perf_counter()
+
+            data["Binary"].append({
+                "Size": current_size,
+                "Time": float(end - begin)
+            })
+
+            begin = time.perf_counter()
+            SearchAlgo.linear_search(search_val, test_array)
+            end = time.perf_counter()
+
+            data["Linear"].append({
+                "Size": current_size,
+                "Time": float(end - begin)
+            })
+            current_size += step
+        with open("SearchingAlgorithmsResults.txt", "w") as outfile:
+            json.dump(data, outfile)
 
 
-def generate_results(min, max, step):
-    current_size = min
-    data = {}
-    data["Binary"] = []
-    data["Linear"] = []
-    while current_size <= max:
-        test_array, search_val = generate_array(current_size)
+    # privremena funkcija za testiranje algoritama
+    def main():
+        print("Generating search algorithms results")
         begin = time.perf_counter()
-        binary_search(search_val, test_array, 0, current_size - 1)
+        SearchAlgo.generate_results(1000000, 10000000, 100000)
         end = time.perf_counter()
+        print(f"Generation finished in: {end - begin:0.4f}s")
 
-        data["Binary"].append({
-            "Size": current_size,
-            "Time": float(end - begin)
-        })
-
-        begin = time.perf_counter()
-        linear_search(search_val, test_array)
-        end = time.perf_counter()
-
-        data["Linear"].append({
-            "Size": current_size,
-            "Time": float(end - begin)
-        })
-        current_size += step
-    with open("SearchingAlgorithmsResults.txt", "w") as outfile:
-        json.dump(data, outfile)
-
-
-# privremena funkcija za testiranje algoritama
-def main():
-    print("Generating search algorithms results")
-    begin = time.perf_counter()
-    generate_results(1000000, 10000000, 100000)
-    end = time.perf_counter()
-    print(f"Generation finished in: {end - begin:0.4f}s")
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
