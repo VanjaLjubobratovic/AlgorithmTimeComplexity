04e4c34aa4ff6623b6042c1d67407e5382755a08
Vanja Ljubobratovic <vanja1708@gmail.com>
2022-01-07 15:59:21 +0100

    Implemented Searching algorithms result generation and saving.

--- main.py
+++ main.py
@@ -5,53 +5,14 @@ from fibonacci import *
 import time
 
 def main ():
-    # ##temporary binary search test
-    # print("BINARY SEARCH")
-    # arr = np.random.randint(100, size=100)
-    # arr[34] = 25
-    # arr.sort()
-    # # print(arr)
-    # print(binary_search(25, arr, 0, len(arr) - 1))
-
-    # temporary linear search test
-    # print("LINEAR SEARCH")
-    # print(linear_search(25, arr))
-
-    # temporary fibonacci test
-    # n = 20
-    # begin = time.perf_counter()
-    # print("FIBONACCI NON-DYNAMIC")
-    # print(fib_non_dynamic(n-1))
-    # end = time.perf_counter()
-
-    # print(f"Time to execute: {end - begin:0.4f} s")
-
-
-    # begin = time.perf_counter()
-    # print("FIBONACCI DYNAMIC")
-    # print(fib_dynamic(n, False))
-    # end = time.perf_counter()
-
-    # print(f"Time to execute: {end - begin:0.4f} s")
-
-    # begin = time.perf_counter()
-    # print("FIBONACCI OPTIMIZED")
-    # print(fib_optimized(n))
-    # end = time.perf_counter()
-
-    # print(f"Time to execute: {end - begin:0.4f} s")
-
-    # generate_search_testcases()
-
-
     # Temporary dijkstra test
    
-    g = Graph.generate_random_graph(1000)
+    g = Graph.generate_random_graph(450)
     begin = time.perf_counter()
     print("DIJKSTRA")
     Graph.dijkstra(g, 0)
-    #print(Graph.dijkstra(g, 0))
-    #print(g.edges)
+    # print(Graph.dijkstra(g, 0))
+    # print(g.edges)
     end = time.perf_counter()
 
     print(f"Time to execute: {end - begin:0.4f} s")
--- searchAlgo.py
+++ searchAlgo.py
@@ -1,5 +1,9 @@
 import json
 import numpy as np
+import time
+import ijson
+
+
 
 def binary_search(value, arr, low, high):
     if high >= low:
@@ -19,24 +23,15 @@ def linear_search(value, arr):
             return i
     return -1
 
-def generate_search_testcases():
-    '''
-    Svaki objekt u datoteci predstavlja jedan testcase i
-    sadrži:
-    Inputs -> lista ulaznih vrijednosti
-    Size -> velicina ulaza
-    Value -> vrijednost koja se traži
-
-    Ideja je da se uvijek traži element na rubovima (worst case)
-    '''
+#OBSOLETE FUNCTION
+def generate_search_testcases(low, high, step):
+    #High should be 10 mil max with this implementation
+    #low is intended to be 1 mil and step is 100k
     data = {}
     data["Searching Algorithms"] = []
-    max = 10000000
-    min = 10
-    step = 10
 
-    i = min
-    while i <= max:
+    i = low
+    while i <= high:
         print(i)
         arr = np.random.randint(i, size=i)
         arr.sort()
@@ -45,6 +40,67 @@ def generate_search_testcases():
             "Inputs": arr.tolist(),
             "Value": int(np.amax(arr))
         })
-        i *= step
+        i += step
     with open("SearchingAlgorithms.txt", "w") as outfile:
         json.dump(data, outfile)
+
+#OBSOLETE FUNCTION
+def read_testcases():
+    test_cases = []
+    test_case_sizes = []
+    test_case_search_values = []
+    with open("SearchingAlgorithms.txt", "rb") as infile:
+        items = ijson.items(infile, "Searching Algorithms.item")
+        for item in items:
+            test_cases.append(item["Inputs"])
+            test_case_sizes.append(item["Size"])
+            test_case_search_values.append(item["Value"])
+    return test_cases, test_case_sizes, test_case_search_values
+
+
+def generate_array(num_of_elements):
+    arr = np.random.randint(num_of_elements, size = num_of_elements)
+    arr.sort()
+    search_val = int(np.amax(arr))
+    return arr, search_val
+
+
+def generate_results(min, max, step):
+    current_size = min
+    data = {}
+    data["Binary"] = []
+    data["Linear"] = []
+    while current_size <= max:
+        test_array, search_val = generate_array(current_size)
+        begin = time.perf_counter()
+        binary_search(search_val, test_array, 0, current_size - 1)
+        end = time.perf_counter()
+
+        data["Binary"].append({
+            "Size": current_size,
+            "Time": float(end - begin)
+        })
+
+        begin = time.perf_counter()
+        linear_search(search_val, test_array)
+        end = time.perf_counter()
+
+        data["Linear"].append({
+            "Size": current_size,
+            "Time": float(end - begin)
+        })
+        current_size += step
+    with open("SearchingAlgorithmsResults.txt", "w") as outfile:
+        json.dump(data, outfile)
+
+
+# privremena funkcija za testiranje algoritama
+def main():
+    print("Generating search algorithms results")
+    begin = time.perf_counter()
+    generate_results(1000000, 10000000, 100000)
+    end = time.perf_counter()
+    print(f"Generation finished in: {end - begin:0.4f}s")
+
+if __name__ == "__main__":
+    main()
